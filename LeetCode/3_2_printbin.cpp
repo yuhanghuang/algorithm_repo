025ab86b0db59ac25deef8f20502ba62b866dc98
yuhanghuang <18691496334@163.com>
2023-03-02 10:05:03 +0800

    二级制转换string.push_back方法补充

--- LeetCode/3_2_printbin.cpp
+++ LeetCode/3_2_printbin.cpp
@@ -46,3 +46,20 @@ int main()
     Solution sl;
     cout<<sl.printBin(num);
 }
+// 方法二
+/*
+class Solution {
+public:
+    string printBin(double num) {
+        string res = "0.";
+        while (res.size() <= 32 && num != 0) {
+            num *= 2;
+            int digit = num;s
+            res.push_back(digit + '0');
+            num -= digit;
+        }
+        return res.size() <= 32 ? res : "ERROR";
+    }
+};
+
+*/
