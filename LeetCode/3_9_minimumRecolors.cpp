2c347c303dc8fd131fa4db56ab1d9116da930a00
yuhanghuang <18691496334@163.com>
2023-03-09 18:53:52 +0800

    滑动窗口补充

--- LeetCode/3_9_minimumRecolors.cpp
+++ LeetCode/3_9_minimumRecolors.cpp
@@ -33,6 +33,26 @@ public:
     }
 
 };
+// 方法二 滑动窗口
+class Solution2 {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int l = 0, r = 0, cnt = 0;
+        while (r < k) {
+            cnt += blocks[r] == 'W' ? 1 : 0;
+            r++;
+        }
+        int res = cnt;
+        while (r < blocks.size()) {
+            cnt += blocks[r] == 'W' ? 1 : 0;
+            cnt -= blocks[l] == 'W' ? 1 : 0;
+            res = min(res, cnt);
+            l++;
+            r++;
+        }
+        return res;
+    }
+};
 
 int main()
 {
