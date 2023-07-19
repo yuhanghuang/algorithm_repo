508ef4b5250747cb8b71173a991949292f19959e
yuhanghuang <18691496334@163.com>
2023-07-19 23:20:28 +0800

    中序遍历修改

--- LeetCode/7_17_inorderTraversal.py
+++ LeetCode/7_17_inorderTraversal.py
@@ -1,8 +1,8 @@
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 #
 # 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 #
@@ -11,13 +11,14 @@
 # @return int整型一维数组
 #
 class Solution:
-    def inorderTraversal(self , root: TreeNode) -> List[int]:
+    def inorderTraversal(self , root: TreeNode) -> list[int]:
         # write code here
         data=[]
         if root==None:
             return []
-        if(root.val==None and root.right==None):
-            data.append(root.val)
+        # 下面可不加
+        # if(root.val==None and root.right==None):
+        #     data.append(root.val)
         data.extend(self.inorderTraversal(root.left))
         data.append(root.val)
         data.extend(self.inorderTraversal(root.right))
