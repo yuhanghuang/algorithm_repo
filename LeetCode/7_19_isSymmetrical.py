13b88368551768d2fa980fbf95ab092ad5a584b8
yuhanghuang <18691496334@163.com>
2023-07-19 14:29:35 +0800

    小修改

--- LeetCode/7_19_isSymmetrical.py
+++ LeetCode/7_19_isSymmetrical.py
@@ -1,11 +1,8 @@
-# class TreeNode:
-
-# from numpy import right_shift
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 # 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 #
 #
